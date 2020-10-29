9fc8c65945e28e1ec92cc99697e053de627ea030
Ariful4593 <arifkhagoria2015@gmail.com>
2020-10-29 13:21:21 +0600

    Pro-Ariful server side code added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("Heloo Ariful ISLAM")
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const clientCollection = client.db("creatingAgency").collection("admin");
+    app.post('/contact', (req, res) => {
+        const details = req.body;
+        clientCollection.insertOne(details)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+        console.log(details)
+    })
+    // agencyCollection.insertOne({ name: 'Ariful Islam' })
+    // perform actions on the collection object
+});
+
+const port = 4000;
+app.listen(port)
